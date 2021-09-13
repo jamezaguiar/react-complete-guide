5734a8b06b58cf9815032c0f37faa39eacb03864
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-09-13 19:52:58 -0300

    Building a First Custom Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+
 const App = () => {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <ExpenseItem />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+const ExpenseItem = () => {
+  return (
+    <div className="expense-item">
+      <h2>Expense Item</h2>
+    </div>
+  );
+};
+
+export default ExpenseItem;
