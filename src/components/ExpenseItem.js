90230cbd20e31e23066c343d90f7a0eb9b8f786c
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-09-13 20:18:13 -0300

    CSS and JSX expressions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,19 @@
+import './ExpenseItem.css';
+
 const ExpenseItem = () => {
+  const expense = {
+    date: new Date(2021, 2, 28),
+    title: 'Car Insurance',
+    amount: 294.67,
+  };
+
   return (
     <div className="expense-item">
-      <h2>Expense Item</h2>
+      <div>{expense.date.toLocaleString('en-US')}</div>
+      <div className="expense-item__description">
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
+      </div>
     </div>
   );
 };
