ac0c16203ca8486396694cfeb54dd9a2d41fd450
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-09-17 08:45:00 -0300

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,9 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expense = {
-    date: new Date(2021, 2, 28),
-    title: 'Car Insurance',
-    amount: 294.67,
-  };
-
+const ExpenseItem = ({ expense }) => {
   return (
     <div className="expense-item">
-      <div>{expense.date.toLocaleString('en-US')}</div>
+      <div>{expense.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
