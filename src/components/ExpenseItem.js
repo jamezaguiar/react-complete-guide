03b323511bb10d0fe603e9113eb42be2de4917a4
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-09-21 21:17:30 -0300

    formatting date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ expense }) => {
+  const month = expense.date.toLocaleString('en-US', { month: 'long' });
+  const day = expense.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = expense.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expense.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
