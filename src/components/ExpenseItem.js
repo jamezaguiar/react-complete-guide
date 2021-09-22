a63fbe87caed40d7386b91d419a13bff655fc654
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-09-22 20:27:44 -0300

    Composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,16 @@
+import Card from './Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ expense }) => {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate expenseDate={expense.date} />
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 };
 
