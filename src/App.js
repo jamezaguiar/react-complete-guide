e9c5b2a78d2347a64c216f4ad45aae92bd583c53
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-15 16:51:22 -0300

    Using stateful lists

--- src/App.js
+++ src/App.js
@@ -5,49 +5,45 @@ import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const [filterYear, setFilterYear] = useState('');
-  const handleSelectYearToFilter = (event) => setFilterYear(event.target.value);
-
-  const allExpenses = useMemo(() => {
-    return [
-      {
-        id: 'e1',
-        title: 'Toilet Paper',
-        amount: 94.12,
-        date: new Date(2020, 7, 14),
-      },
-      {
-        id: 'e2',
-        title: 'New TV',
-        amount: 799.49,
-        date: new Date(2021, 2, 12),
-      },
-      {
-        id: 'e3',
-        title: 'Car Insurance',
-        amount: 294.67,
-        date: new Date(2021, 2, 28),
-      },
-      {
-        id: 'e4',
-        title: 'New Desk (Wooden)',
-        amount: 450,
-        date: new Date(2021, 5, 12),
-      },
-    ];
-  }, []);
+  const [allExpenses, setAllExpenses] = useState([
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ]);
 
   const expenses = useMemo(() => {
     if (isEmpty(filterYear)) return allExpenses;
 
     return allExpenses.filter(
-      (expense) => expense.date.getFullYear().toString() === filterYear
+      expense => expense.date.getFullYear().toString() === filterYear
     );
   }, [allExpenses, filterYear]);
 
-  const addExpenseHandler = (expenses) => {
-    console.log('In App.js');
-    console.log(expenses);
-  };
+  const handleSelectYearToFilter = event => setFilterYear(event.target.value);
+
+  const addExpenseHandler = newExpense =>
+    setAllExpenses(prevExpenses => [...prevExpenses, newExpense]);
 
   return (
     <div>
